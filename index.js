7a30058dbfd290d6c809cc4951aa7d49e51d8063
Syful Islam <48632407+syfulislambappi@users.noreply.github.com>
2022-10-22 23:00:44 +0600

    basic server setup

--- index.js
+++ index.js
@@ -1,44 +1,10 @@
-// Dependencies
-const express = require("express");
-const mongoose = require("mongoose");
-const errorHandler = require("./middlewares/errorHandler");
+require("dotenv").config();
+const http = require("http");
+const app = require("./app/app");
 
-const app = express();
+const server = http.createServer(app);
 const PORT = process.env.PORT || 8080;
 
-// Application middlewares
-app.use([express.json()]);
-
-// Set view engine
-app.set("view engine", "ejs");
-
-// Main routes
-app.get("/", (req, res) => res.status(200).render("index"));
-
-// Application error handler
-app.use(errorHandler);
-
-// Connect database
-const main = async () => {
-  try {
-    await mongoose.connect("mongodb://127.0.0.1:27017/poll");
-    console.log(`Database is connected.`);
-    // Create server
-    app.listen(PORT, () => {
-      console.log(`Server is running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.log(err.message);
-  }
-};
-
-// Global error handler
-process.on("unhandledRejection", (error) => {
-  console.log(error.name, error.message);
-  app.close(() => {
-    process.exit(1);
-  });
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}.`);
 });
-
-// Call the database connection and server connection
-main();
